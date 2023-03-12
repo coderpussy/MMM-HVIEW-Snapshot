3602f1f595b8c53081a1bb5b0de62801a95ace2a
maja <maja-aurora@tesserakt.com>
2023-03-12 14:02:03 +0100

    Added HTML template. Removed animation speed.
    Removed node_helper.js

--- MMM-HVIEW-Snapshot.js
+++ MMM-HVIEW-Snapshot.js
@@ -2,7 +2,8 @@
 
 Module.register("MMM-HVIEW-Snapshot",{
 	defaults: {
-		animationSpeed: 500,
+        id: "camera01",
+        url: "",
 		updateInterval: 60000,
         wrapperWidth: "640px",
         wrapperHeight: "360px",
@@ -13,51 +14,20 @@ Module.register("MMM-HVIEW-Snapshot",{
 	},
 
 	start: function() {
-		let self = this;
-        
 		setInterval(() => {
-            // Start helper and data polling
-			self.sendSocketNotification("CONFIG", self.config);
-		}, self.config.updateInterval);
-	},
-    
-	getDom: function() {
-        let self = this;
-        
-        var wrapper = document.createElement("div");
-        wrapper.className = "wrap";
-        wrapper.style.width = self.config.wrapperWidth;
-        wrapper.style.height = self.config.wrapperHeight;
-        
-        var makeIframe = document.createElement("iframe");
-        makeIframe.id = self.config.id; 
-        makeIframe.className = "frame";
-        makeIframe.setAttribute("scrolling", "no");
-        makeIframe.src = self.config.url;
-        makeIframe.style.width = self.config.iframeWidth;
-        makeIframe.style.height = self.config.iframeHeight;
-        makeIframe.style.transform = "scale(" + self.config.scale + ")";
-        makeIframe.style.filter = "grayscale(" + self.config.grayscale + ")";
-        
-        var hidecursordiv = document.createElement("div");
-        hidecursordiv.className = "hide-cursor";
-        
-        wrapper.appendChild(makeIframe);
-        wrapper.appendChild(hidecursordiv);
-        
-		return wrapper;
+            this.updateDom();
+		}, this.config.updateInterval);
 	},
 
-	getScripts: function() {
-		return ["MMM-HVIEW-Snapshot.css"];
+	getStyles: function() {
+		return ['MMM-HVIEW-Snapshot.css'];
 	},
     
-    // Handle socket answer
-    socketNotificationReceived: function(notification, payload) {
-        Log.info(this.name + " received a notification: " + notification);
-        // Care only own socket answers
-        if (notification === "MMM_HVIEW_SNAPSHOT_IFRAME_UPDATE") {
-            this.updateDom(self.config.animationSpeed);
-        }
-    }
+    getTemplate: function () {
+        return 'MMM-HVIEW-Snapshot.njk';
+	},
+	
+    getTemplateData: function () {
+        return templateData = { config: this.config };
+	}
 });
